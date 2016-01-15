20416200d53e2613ac94bc79188e374f23fc9e37
Joe Van Dyk <joe@tanga.com>
2016-01-15 05:17:52 -0800

    add a test

--- spec/wisper/que_spec.rb
+++ spec/wisper/que_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
+require 'pg'
+
+Que.connection = PG::Connection.open("postgres://localhost/wisper-que-test")
+Que.mode = :sync
 
 describe Wisper::Que do
   it 'has a version number' do
     expect(Wisper::Que::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  # TODO come up with a test that's not stupid.
+
+  class Joe
+    include Wisper::Publisher
+    def call
+      broadcast(:sup, 'joe')
+    end
+  end
+
+  class SomeJob
+    extend Wisper::Publisher
+    def self.sup(args)
+      $result ||= []
+      $result << "i got #{args}"
+    end
+  end
+
+  it 'calls SomeJob' do
+    joe = Joe.new
+    joe.subscribe(SomeJob, async: true)
+    joe.subscribe(SomeJob, que: true)
+    joe.call
+
+    expect($result).to be == ["i got joe", "i got joe"]
   end
 end
